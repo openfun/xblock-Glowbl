dfd83d25df3fee7f0742e1c208b0522692df1804
Richard Moch <richard.moch@gmail.com>
2019-01-22 11:45:45 +0100

    💬(text) update text informing user of terms of glowbl service

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def package_data(pkg, roots):
 
 setup(
     name='fun_glowbl-xblock',
-    version='0.1',
+    version='0.1.1',
     description='fun_glowbl XBlock',   # TODO: write a better description.
     license='License :: OSI Approved :: GNU Affero General Public License v3',
     packages=[
